92869763c161c804758b5d87fdc0864c8134a03f
Adrien Lemaire <dori@ikkyotech.com>
2015-02-21 15:06:19 +0900

    remove docker build step. Now handled with 'make build'

--- dependencies.sh
+++ dependencies.sh
@@ -6,22 +6,14 @@
 repo=$1
 mv circleci_shared/* .
 
-# Move src code to current dir
-mv ../src/ .
-
-# Clone etl_shared
-git clone git@github.com:AlpacaDB/etl_shared.git
-cd etl_shared && git checkout $GIT_BRANCH && cd ..
-
 # Connect to remove docker registry
 sudo cp registry.crt /usr/local/share/ca-certificates
 sudo update-ca-certificates
 sudo service docker restart
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS $EXTERNAL_REGISTRY_ENDPOINT
 
-# Build repository
-docker pull $EXTERNAL_REGISTRY_ENDPOINT/$repo
-docker build -t $EXTERNAL_REGISTRY_ENDPOINT/$repo:$CIRCLE_SHA1 .
+# Create tag for the docker image
+docker tag $repo $EXTERNAL_REGISTRY_ENDPOINT/$repo:$CIRCLE_SHA1
 
 # Connect to remote kubernetes apiserver
 wget https://storage.googleapis.com/kubernetes-release/release/v0.9.1/bin/linux/amd64/kubectl -O ~/kubectl
